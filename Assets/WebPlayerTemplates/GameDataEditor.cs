c56dbe9b527ed5486c4806bcba9942e0fb35b9a7
8BITSKY-PC\8BitSky <cruiseslater@gmail.com>
2020-02-23 20:40:20 -0600

    Prior to Dev Build 1.0

--- Assets/WebPlayerTemplates/GameDataEditor.cs
+++ Assets/WebPlayerTemplates/GameDataEditor.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿#if (UNITY_EDITOR) 
+using UnityEngine;
 using UnityEditor;
 using System.Collections;
 using System.IO;
@@ -63,3 +64,4 @@ public class GameDataEditor : EditorWindow
 
     }
 }
+#endif
